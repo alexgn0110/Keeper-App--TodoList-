12349404c1a50a44916a73e4a3fdc15f94ac4cb9
Alex GN <alex_guno0110@hotmail.com>
2022-10-19 21:49:28 -0500

    First and only commit

---
+++ keeper-app-agn/src/componets/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FormatListNumberedIcon/>
+        Keeper App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
